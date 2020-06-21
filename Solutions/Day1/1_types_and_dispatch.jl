1f1cfd42a768c569be08b102123144a69894ecd5
de-souza <leo@de-souza.eu>
2020-06-21 14:33:39 +0200

    Minor formatting tweaks

--- Solutions/Day1/1_types_and_dispatch.jl
+++ Solutions/Day1/1_types_and_dispatch.jl
@@ -1,13 +1,20 @@
-println(
-    "Function that takes any real matrix as an input and returns the element "
-    * "type of the matrix:"
-)
+println("""
+    Create a function that takes any real matrix as an input and returns
+    the element type of the matrix:
+""")
+
 realmatrixeltype(x::Matrix{<:Real}) = eltype(x)
 display(realmatrixeltype)
 
-println("\nMatrix filled with random integers from 0 to 9:")
+println("""
+    Matrix m filled with random integers from 0 to 9:
+""")
+
 m = rand(0:9, 3, 3)
 display(m)
 
-println("\nElement type of m:")
+println("""
+    Element type of m:
+""")
+
 display(realmatrixeltype(m))
--- Solutions/Day1/2_custom_types.jl
+++ Solutions/Day1/2_custom_types.jl
@@ -44,16 +44,15 @@ length(v::OneHot) = Int(v.length)
 function innersum(A, vs)
     t = zero(eltype(A)) # generic!
     for v in vs
-        y = A*v
-        for i in 1:length(vs[1])
-            # t += v[i]
+        y = A * v
+        for i = 1:length(vs[1])
             t += v[i] * y[i]
         end
     end
     return t
 end
 
-A = rand(3,3)
-vs = [OneHot(rand(1:3), 3) for _=1:10]
+A = rand(3, 3)
+vs = [OneHot(rand(1:3), 3) for _ = 1:10]
 
 display(innersum(A, vs))
